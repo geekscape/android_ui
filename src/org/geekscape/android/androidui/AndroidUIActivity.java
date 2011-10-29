ea3437e1f8c5fc007a71f4b490814a45f7bea0b8
Andy Gelme <andyg@geekscape.org>
2011-10-29 17:00:29 +1100

    Minor change to log message.

--- src/org/geekscape/android/androidui/AndroidUIActivity.java
+++ src/org/geekscape/android/androidui/AndroidUIActivity.java
@@ -6,13 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class AndroidUIActivity extends Activity {
-  public static String DEBUG_TAG = "AndroidUI";
+  public static String LOG_TAG = AndroidUIActivity.class.getSimpleName();
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(LOG_TAG, "onCreate()");
         setContentView(R.layout.main);
-        Log.i(DEBUG_TAG, "Log() test");
     }
 }
